bb16d91bc2c73aed1be3670a9bd02514c7d94d4d
jorgenieva23 <jorge_4755@hotmail.com>
2025-05-15 10:00:00 -0300

    Descripción de los cambios realizaddos

---
+++ src/presentation/components/ui/FadeInImage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const FadeInImage = () => {
+  return (
+    <View>
+      <Text>FadeInImage</Text>
+    </View>
+  )
+}
